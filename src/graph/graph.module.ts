8a0015808b7960f3ba76403160a899655b08cccd
Daniel Biton <daniel.biton@abra-it.com>
2025-11-04 11:54:09 +0200

    added webhook successfully

--- src/graph/graph.module.ts
+++ src/graph/graph.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphController } from './graph.controller';
 import { GraphService } from './graph.service';
+import { WebhookController } from "./webhook.controller";
 
 @Module({
-  controllers: [GraphController],
-  providers: [GraphService]
+  controllers: [GraphController, WebhookController],
+  providers: [GraphService],
+  exports: [GraphService]
 })
 export class GraphModule {}
